f14b4c7779c5a1a7ab8b6c603a4cf8c1e610f0b3
Anthony Lucci <totowdu80@gmail.com>
2020-06-23 11:36:19 +0200

    refactor: update the map background

--- src/components/Map/MyMap.jsx
+++ src/components/Map/MyMap.jsx
@@ -30,17 +30,16 @@ const defaultFluctuoAPI = gql`
 `;
 
 function MyMap() {
-
   const [lat, setLat] = useState(44.8420873);
   const [lng, setLng] = useState(-0.5754373);
-  const [zoom, setZoom] = useState(18);
+  /* const [zoom, setZoom] = useState(18); */
   const [fluctuoAPI, setFluctuoAPI] = useState(defaultFluctuoAPI);
 
-  const position = [lat,lng];
-  const { loading, error, data } = useQuery(fluctuoAPI);
+  const position = [lat, lng];
+  const { data } = useQuery(fluctuoAPI);
 
   useEffect(() => {
-    const posMarker = [44.8420873,-0.5754373];
+    const posMarker = [44.8420873, -0.5754373];
     const fluctuoAPI = gql`
     {
       vehicles (lat: ${posMarker[0]}, lng: ${posMarker[1]}) {
@@ -62,10 +61,10 @@ function MyMap() {
   }, [data]);
 
   return (
-    <Map className="map" center={position} zoom="18"/*zoom={zoom}*/ onViewportChange={(viewport) => { setLat(viewport.center[0]); setLng(viewport.center[1])}}>
+    <Map className="map" center={position} zoom="18" onViewportChange={(viewport) => { setLat(viewport.center[0]); setLng(viewport.center[1])}}>
       <TileLayer
         attribution="© <a href='https://www.mapbox.com/about/maps/%27%3EMapbox</a> © <a href='http://www.openstreetmap.org/copyright%27%3EOpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
-        url="https://api.mapbox.com/styles/v1/brianlag/ckbrnsqyh327j1iloa35gzxos/tiles/256/%7Bz%7D/%7Bx%7D/%7By%7D@2x?access_token=pk.eyJ1IjoiYnJpYW5sYWciLCJhIjoiY2ticmx5anhtMnhzMjJ4bDluM2N3dzJxMSJ9.3ynE8Sf5t0N7PdHaZukyDw"
+        url="https://api.mapbox.com/styles/v1/brianlag/ckbrnsqyh327j1iloa35gzxos/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYnJpYW5sYWciLCJhIjoiY2ticmx5anhtMnhzMjJ4bDluM2N3dzJxMSJ9.3ynE8Sf5t0N7PdHaZukyDw"
       />
       {data && data.vehicles.map((vehicle) => (
         <Marker position={[vehicle.lat, vehicle.lng]} icon={myIcon} data={data}>
