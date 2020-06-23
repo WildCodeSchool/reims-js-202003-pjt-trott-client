bcfbb3a3798390bd9e86280781977457ea0a93db
Anthony Lucci <totowdu80@gmail.com>
2020-06-23 10:33:40 +0200

    refactor: update the map and the markers call

--- src/components/Map/MyMap.jsx
+++ src/components/Map/MyMap.jsx
@@ -1,25 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import ApolloClient from 'apollo-boost';
 import { gql } from 'apollo-boost';
-import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 import L from 'leaflet';
-import MarkerList from './MarkerList'
 import './MyMap.css';
 
 const myIcon = L.icon({
   iconUrl: 'https://www.scafom-echafaudage.fr/wp-content/uploads/2018/10/marker_2-512.png',
   iconSize: [60, 50],
   iconAnchor: [12.5, 41],
-  popupAnchor: [0, -41]
-})
-
-const client = new ApolloClient({
-  uri : 'http://flow-api.fluctuo.com/v1?access_token=g8bGqo424KsGKVRw8wxOZ7oGdWmM4lyL'
+  popupAnchor: [0, -41],
 });
 
-const posMarker = [44.8420873,-0.5754373];
+const posMarker = [44.8420873, -0.5754373];
 const defaultFluctuoAPI = gql`
 {
   vehicles (lat: ${posMarker[0]}, lng: ${posMarker[1]}) {
@@ -68,27 +62,24 @@ function MyMap() {
   }, [data]);
 
   return (
-    <ApolloProvider client={client}>
-      <Map className="map" center={position} zoom="18"/*zoom={zoom}*/ onViewportChange={(viewport) => { setLat(viewport.center[0]); setLng(viewport.center[1])}}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright%22%3EOpenStreetMap">Toto</a> contributors'
-          url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/*<MarkerList data={data} /> */}
-        {data && data.vehicles.map((vehicle) => (
-          <Marker position={[vehicle.lat, vehicle.lng]} icon={myIcon} data={data}> 
-            <Popup>
-              You are here
-            </Popup>
-          </Marker>
-        ))}
-        <Marker position={posMarker} icon={myIcon} data={data}> 
+    <Map className="map" center={position} zoom="18"/*zoom={zoom}*/ onViewportChange={(viewport) => { setLat(viewport.center[0]); setLng(viewport.center[1])}}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright%22%3EOpenStreetMap">Toto</a> contributors'
+        url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {data && data.vehicles.map((vehicle) => (
+        <Marker position={[vehicle.lat, vehicle.lng]} icon={myIcon} data={data}>
           <Popup>
             You are here
           </Popup>
         </Marker>
-      </Map>
-    </ApolloProvider>
+      ))}
+      <Marker position={posMarker} icon={myIcon} data={data}>
+        <Popup>
+          You are here
+        </Popup>
+      </Marker>
+    </Map>
   );
 }
 
