2d779f83be1bbd619b3f6aafe3676b6125f15832
Brian <brian.lagaude@gmail.com>
2020-06-30 10:35:40 +0200

    feat: create Login class

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Login extends React.Component{
+
+render(){
+  return(
+    <div className='loginDiv'>
+      <h1>Connexion utilisateur</h1>
+    </div>
+  );
+}
+}
