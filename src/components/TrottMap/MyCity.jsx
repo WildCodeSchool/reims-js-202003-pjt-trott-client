439003934f56725c66e53db675c0966a363064bb
malikH88 <malik.hyamine@gmail.com>
2020-07-03 11:41:26 +0200

    feat(scan page): add link in map for scan page

--- src/components/TrottMap/MyCity.jsx
+++ src/components/TrottMap/MyCity.jsx
@@ -8,7 +8,7 @@ const MyCity = () => {
       <div className="Page_Container">
         <div className="Home_Title_container">
           <div className="Flex_Title">
-            <h1 className="Home_Title">Bienvenue à Bordeaux !</h1>
+            <h1 className="Home_Title">Bienvenue à Paris !</h1>
           </div>
         </div>
         <div className="Logo_Container">
