5c1957badd1389900e61b2c01aec043bd696924c
Anthony Lucci <totowdu80@gmail.com>
2020-07-03 11:07:59 +0200

    refactor: huge update of front

--- src/components/Instructions/InstruPageTwo.jsx
+++ src/components/Instructions/InstruPageTwo.jsx
@@ -1,80 +1,51 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import RankStats from '../Divers/RankStats';
 
 const InstruPageTwo = () => (
-  <div className="body_rank">
-    <p className="text_rank">
+  <div className="bg_instruPageTwo">
+    <p className="block_text_rank">
       Rejoins la communauté et monte dans le classement pour obtenir des courses gratuites
       chez tes marques préferées,et d'autres cadeaux !
     </p>
 
-    <div className="first_rank">
+    <div className="block_podium">
 
-      <div className="round2">
-        <img src="https://via.placeholder.com/150" alt=" user" />
+      <div className="block_player">
+        <img className="img_player img_second" src="https://via.placeholder.com/150" alt=" user" />
         <p className="text_rank">2</p>
         <p className="text_rank">Matthieu</p>
-        <div className="user2_score">
-          <p className="score_text">590</p>
+        <div className="bg_scoreBlock bg_scoreOne">
+          <p>590</p>
         </div>
       </div>
 
-      <div className="round">
-        <img src="https://via.placeholder.com/150" alt=" user" />
+      <div className="block_player">
+        <img className="img_player img_first" src="https://via.placeholder.com/150" alt=" user" />
         <p className="text_rank">1</p>
         <p className="text_rank">Kylian Mbappé</p>
-        <div className="user1_score">
+        <div className="bg_scoreBlock bg_scoreTwo">
           <p className="score_text">2420</p>
         </div>
       </div>
 
-      <div className="round2">
-        <img src="https://via.placeholder.com/150" alt=" user" />
+      <div className="block_player">
+        <img className="img_player img_third" src="https://via.placeholder.com/150" alt=" user" />
         <p className="text_rank">3</p>
         <p className="text_rank">Mathilda R</p>
-        <div className="user3_score">
+        <div className="bg_scoreBlock bg_scoreThree">
           <p className="score_text">309</p>
         </div>
       </div>
-
     </div>
-
-    <div className="other_ranks">
-      4
-      <div className="rank_info">
-        <img src="https://via.placeholder.com/50" alt=" user" />
-        Jerome Niel
-        <div className="rank_score">
-          214
-        </div>
-      </div>
+    <div className="block_underPodium">
+      <RankStats position="4" name="Jérome Niel" score="214" img="https://via.placeholder.com/50" />
+      <RankStats position="5" name="Studio Danielle" score="189" img="https://via.placeholder.com/50" />
+      <RankStats position="6" name="Le Professeur" score="78" img="https://via.placeholder.com/50" />
     </div>
-
-    <div className="other_ranks">
-      5
-      <div className="rank_info">
-        <img src="https://via.placeholder.com/50" alt=" user" />
-        Studio Danielle
-        <div className="rank_score">
-          189
-        </div>
-      </div>
+    <div className="linkButton_block">
+      <Link className="link_button" to="/register">C'est Parti</Link>
     </div>
-
-    <div className="other_ranks_3">
-      4
-      <div className="rank_info">
-        <img src="https://via.placeholder.com/50" alt=" user" />
-        Le Professeur
-        <div className="rank_score">
-          78
-        </div>
-      </div>
-    </div>
-
-    <Link className="button_rank" to="/register">C'est Parti</Link>
-
   </div>
 );
 
