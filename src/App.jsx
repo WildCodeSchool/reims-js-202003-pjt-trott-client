d9b37fdd1dc99d1b86e63aed28a643171ebc6849
Brian <brian.lagaude@gmail.com>
2020-07-07 09:30:52 +0200

    refactor: add css on Login

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,8 @@ import InstructionsIndex from './components/Instructions/InstructionsIndex';
 import RegisterIndex from './components/Register/RegisterIndex';
 import Login from './components/Login/Login';
 import TrottIndex from './components/TrottMap/TrottIndex';
-<<<<<<< HEAD
 import Menu from './components/Menu/Menu';
-=======
-import Scan from './components/TrottMap/Scan'
->>>>>>> d1d15c74f1bdb11e9740fdac669c74fc4781a0e8
+import Scan from './components/TrottMap/Scan';
 
 function App() {
   return (
