4d990389a4e0890760e6282a44eee82a6c1bbd8c
malikH88 <malik.hyamine@gmail.com>
2020-07-10 11:30:30 +0200

    refactor(scan page): add css

--- src/components/Trottinette/Trottinette2.jsx
+++ src/components/Trottinette/Trottinette2.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import troot1 from "../../Image/bird.jpg";
+import { Link } from 'react-router-dom';
 
 const Trottinette2 = () => (
-  <div>
-    <h1>Trottinette</h1>
-    <p>Marque : Bird</p>
-    <p>autonomie : 68%</p>
-    <p>Prix : 2$</p>
+  <div className="container_Content">
+    <h1 className="title_troot">Trottinette</h1>
+    <img className='Preview_troot' src={troot1} alt="gift" />
+    <p>Marque : <strong>Bird</strong></p>
+    <p>Autonomie : <strong>65%</strong></p>
+    <p>Prix : <strong>2€</strong></p>
+    <Link to="/register" className='link_button'>Déverouillez</Link>
   </div>
 );
 
