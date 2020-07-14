8f8c4de0f34d270dd4a4e3780f3b83168f2bdc89
Brian <brian.lagaude@gmail.com>
2020-07-14 17:24:03 +0200

    refactor: refine css on menu burger

--- src/components/TrottMap/Menu.jsx
+++ src/components/TrottMap/Menu.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import User from '../../Image/user.png';
 import Gift from '../../Image/gift.png';
 import BurgerMenu from '../../Image/menu.png';
+import { useEffect } from 'react';
+
+
+/* const mapStatetoProps = (state) => ({
+  token : state.token,
+});
+
+const NoConnect = ({ token, history }) => {
+  useEffect(() => {
+    if (token === null)
+  })
+} */
 
 function Burger() {
   // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
@@ -44,9 +57,6 @@ function Burger() {
         </div>
         <div className="user_list">
           Environnement
-        </div>
-        <div className="user_list">
-          Discover
         </div>
       </div>
     </Menu>
