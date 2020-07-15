b439d0d3a2440438c41d58f42d1bf4d04f737a03
Brian <brian.lagaude@gmail.com>
2020-07-15 11:10:06 +0200

    refactor: add conditionnal menu or popup if user is connect

--- src/components/TrottMap/Menu.jsx
+++ src/components/TrottMap/Menu.jsx
@@ -2,57 +2,66 @@ import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { ToastContainer, toast } from 'react-toastify';
 import User from '../../Image/user.png';
 import Gift from '../../Image/gift.png';
 import BurgerMenu from '../../Image/menu.png';
-
+import 'react-toastify/dist/ReactToastify.css';
 
 const mapStateToProps = (state) => ({
   token: state.token,
 });
 
 
-function Burger() {
-  // NOTE: You also need to provide styles, see https://github.com/negomi/react-burger-menu#styling
-  return (
-    <Menu width="100vw" customBurgerIcon={<img src={BurgerMenu} alt="icon" />} className="size">
-      <div className="menu_infos">
-        <div className="menu_flex">
-          <div className="menu_info_1">
-            <div>
-              <p className="menu_info_important">4</p>
-              courses effectués
+function Burger(props) {
+  const notify = () => toast('Wow so easy !');
+
+  if (props.token !== null) {
+    return (
+      <Menu width="100vw" customBurgerIcon={<img src={BurgerMenu} alt="icon" />} className="size">
+        <div className="menu_infos">
+          <div className="menu_flex">
+            <div className="menu_info_1">
+              <div>
+                <p className="menu_info_important">4</p>
+                courses effectués
+              </div>
+            </div>
+            <div className="menu_info_1">
+              <p className="menu_info_important">#1</p>
+              <div className="menu_info_text">
+                Classement général
+              </div>
+            </div>
+            <div className="menu_info_3">
+              <p className="menu_info_important">1,2</p>
+              Arbres Plantés
             </div>
           </div>
-          <div className="menu_info_1">
-            <p className="menu_info_important">#1</p>
-            <div className="menu_info_text">
-              Classement général
-            </div>
+          <div className="user_list">
+            <Link className="linkdeco" to="/map/menu">
+              <img src={User} alt="user" />
+              Mon compte
+            </Link>
           </div>
-          <div className="menu_info_3">
-            <p className="menu_info_important">1,2</p>
-            Arbres Plantés
+          <div className="user_list">
+            Les défis
+          </div>
+          <div className="user_list">
+            <img src={Gift} alt="gift" />
+            Parrainage
+          </div>
+          <div className="user_list">
+            Environnement
           </div>
         </div>
-        <div className="user_list">
-          <Link className="linkdeco" to="/map/menu">
-            <img src={User} alt="user" />
-            Mon compte
-          </Link>
-        </div>
-        <div className="user_list">
-          Les défis
-        </div>
-        <div className="user_list">
-          <img src={Gift} alt="gift" />
-          Parrainage
-        </div>
-        <div className="user_list">
-          Environnement
-        </div>
-      </div>
-    </Menu>
+      </Menu>
+    );
+  } return (
+    <>
+      <img className="img-menu-no-connect" onClick={notify} src={BurgerMenu} alt="notify" />
+      <ToastContainer />
+    </>
   );
 }
 
