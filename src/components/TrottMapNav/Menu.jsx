d39d693ed79224013224119c91bab3516fb581a5
Brian <brian.lagaude@gmail.com>
2020-07-17 10:30:35 +0200

    feat: add new icons to menu

--- src/components/TrottMapNav/Menu.jsx
+++ src/components/TrottMapNav/Menu.jsx
@@ -6,16 +6,15 @@ import { ToastContainer, toast } from 'react-toastify';
 import User from '../../Image/user.png';
 import Gift from '../../Image/gift.png';
 import BurgerMenu from '../../Image/menu.png';
+import Tree from '../../Image/tree2x.png';
 import 'react-toastify/dist/ReactToastify.css';
 
 const mapStateToProps = (state) => ({
   token: state.token,
 });
 
-
 function Burger(props) {
   const notify = () => toast.info('Tu dois être connecté pour ouvrir le menu !');
-
 
   if (props.token !== null) {
     return (
@@ -41,18 +40,20 @@ function Burger(props) {
           </div>
           <div className="user_list">
             <Link className="linkdeco" to="/map/menu">
-              <img src={User} alt="user" />
+              <img className="menuImage" src={User} alt="user" />
               Mon compte
             </Link>
           </div>
           <div className="user_list">
+            <img className="menuImage" src={User} alt="défi" />
             Les défis
           </div>
           <div className="user_list">
-            <img src={Gift} alt="gift" />
+            <img className="menuImage" src={Gift} alt="gift" />
             Parrainage
           </div>
           <div className="user_list">
+            <img className="menuImage" src={Tree} alt="tree" />
             Environnement
           </div>
         </div>
@@ -66,17 +67,19 @@ function Burger(props) {
           <p className="user_no_stat">  Se connecter pour voir les statistiques</p>
         </div>
         <div onClick={notify} className="user_list">
-          <img src={User} alt="user" />
+          <img className="menuImage" src={User} alt="user" />
           Mon compte
         </div>
         <div onClick={notify} className="user_list">
+          <img className="menuImage" src={User} alt="défi" />
           Les défis
         </div>
         <div onClick={notify} className="user_list">
-          <img src={Gift} alt="gift" />
+          <img className="menuImage" src={Gift} alt="gift" />
           Parrainage
         </div>
         <div onClick={notify} className="user_list">
+          <img className="menuImage" src={Tree} alt="tree" />
           Environnement
         </div>
         <div className="user_list">
